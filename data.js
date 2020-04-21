0f807cdbac2655b0abd83d486c5a4931214f8cd0
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-04-20 18:11:37 -0700

    week 1 code

---
+++ data.js
@@ -0,0 +1,32 @@
+let DragonAgeNovels = [
+
+    { title: 'The Stolen Throne',
+      author: 'David Gaider',
+      year: 2009,
+      pages: 400 },
+
+    { title: 'The Calling',
+      author: 'David Gaider',
+      year: 2009,
+      pages: 448 },
+
+    { title: 'Asunder',
+      author: 'David Gaider', 
+      year: 2011,
+      pages: 448 },
+
+    { title: 'The Masked Empire',
+      author: 'Patrick Weekes',
+      year: 2014,
+      pages: 384 },
+
+    { title: 'Last Flight',
+      author: 'Liane Merciel',
+      year: 2014,
+      pages: 301 }
+
+]
+
+exports.DragonAgeNovels = DragonAgeNovels;
+
+exports.amtNovels = DragonAgeNovels.length;
---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http"), fs = require("fs"), nov = require("./data");
+
+http.createServer( (req,res) => {  // callback, function to be invoked later
+
+    const path = req.url.toLocaleLowerCase();
+
+    switch(path) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(`There are ${nov.amtNovels} novels in the Dragon Age series.`);  // http://localhost:3000
+            break;
+
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(`My name is Kira Abell. I am working toward my AAS in web development at Seattle Central College.`);  // http://localhost:3000/about
+            break;
+
+        // case '/ex':
+        //     fs.readFile( "ex.html", (err, html) => {
+        //         if (err) return console.error(err);
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write(html);
+        //         res.end();
+        //     } );
+        //     break;
+        // -- save for later!! import and load an html file
+
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end(`404 Error`);
+            break;
+
+    }
+
+} ).listen(process.env.PORT || 3000);
