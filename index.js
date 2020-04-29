2604a4de4f4c0ce58dde55fe7da0efc7c4ff4835
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-04-29 13:09:56 -0700

    cleanup and version correction
    deletes unnecessary comments in index
    bumps version to 1.0.1 for wk1 hw -- all versions will now follow this pattern from week to week for each successive hw

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http"), fs = require("fs"), novels = require("./lib/data.js");
+const http = require("http"), novels = require("./lib/data.js");
 
 http.createServer( (req,res) => {  // callback, function to be invoked later
 
@@ -14,16 +14,6 @@ http.createServer( (req,res) => {  // callback, function to be invoked later
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(`My name is Kira Abell. I am working toward my AAS in web development at Seattle Central College.`);  // http://localhost:3000/about
             break;
-
-        // case '/ex':
-        //     fs.readFile( "ex.html", (err, html) => {
-        //         if (err) return console.error(err);
-        //         res.writeHead(200, {'Content-Type': 'text/html'});
-        //         res.write(html);
-        //         res.end();
-        //     } );
-        //     break;
-        // -- save for later!! import and load an html file
 
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
